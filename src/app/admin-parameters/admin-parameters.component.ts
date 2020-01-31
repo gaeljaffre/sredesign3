ae3b2fb58c436ccc4acb2662ea35f91f969eff75
gaeljaffre <gael.jaffre@yahoo.fr>
2020-01-31 22:31:14 +0100

    3 files modified

--- src/app/admin-parameters/admin-parameters.component.ts
+++ src/app/admin-parameters/admin-parameters.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { urlBackend } from '../config-bd';
+
 @Component({
   selector: 'app-admin-parameters',
   templateUrl: './admin-parameters.component.html',
   styleUrls: ['./admin-parameters.component.css']
 })
 export class AdminParametersComponent implements OnInit {
+
+  url = urlBackend;
 
   constructor() { }
 
